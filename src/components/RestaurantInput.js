7a8c60eae5574bfb3cd03b05b13c9d2b090318e6
earthctzn <earthctzn1@gmail.com>
2020-02-08 20:18:01 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
+import { addRestaurant } from '../actions/restaurants';
+
 
 export class RestaurantInput extends Component {
 
@@ -8,7 +9,6 @@ export class RestaurantInput extends Component {
     name: '',
     location: ''
   }
-
   handleOnNameChange = event => {
     this.setState({
       name: event.target.value
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +49,12 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = (state) => {
+  return {
+    restaurants: state.restaurants
+  };
+};
+
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default  connect( mapStateToProps, { addRestaurant })(RestaurantInput)
